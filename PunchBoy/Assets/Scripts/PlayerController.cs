626a96afb8c797eec96fcd97044496744b2bb636
BrandonBaier-png <144815527+BrandonBaier-png@users.noreply.github.com>
2024-02-22 23:15:18 -0600

    Basic Punch Boy Controls
    Created a Punch Boy playerObject and PlayerController to use for later. Attempted implementation of basic grid teleportation mechanics

---
+++ PunchBoy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float tileMove = 500;
+    public Transform movePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            MovePlayer(Vector3.forward);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            MovePlayer(Vector3.left);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            MovePlayer(Vector3.back);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            MovePlayer(Vector3.right);
+        }
+    }
+
+    private void MovePlayer(Vector3 direction)
+    {
+        transform.position += (direction * tileMove * Time.deltaTime);
+        //transform.Translate(direction * tileMove * Time.deltaTime);
+    }
+
+
+}
