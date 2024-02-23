aefd64c3189b4e8807d7f9624ce82862983abb5d
BrandonBaier-png <baierb1283@my.uwstout.edu>
2024-02-23 14:00:44 -0600

    Basic grid implementation created
    Testing around with the creation of grid space for punch boy to move between, spawns upon runtime
    (Functional)

---
+++ PunchBoy/Assets/Scripts/gridLogic.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class gridLogic : MonoBehaviour
+{
+    [SerializeField] private Grid grid;
+    [SerializeField] private Vector3Int gridSize;
+    [SerializeField] private Object tilePrefab;
+    [SerializeField] private int gridHeight;
+    MeshRenderer myRenderer; //= GetComponent<MeshRenderer>();
+    [SerializeField] Material whiteMaterial;// = Resources.Load("white", typeof(Material)) as Material;
+    [SerializeField] Material blackMaterial;// = Resources.Load("white", typeof(Material)) as Material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var worldPosition = grid.GetCellCenterWorld(new Vector3Int(0, 1));
+        //Instantiate(tilePrefab, worldPosition, Quaternion.identity)
+        Generate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void Generate()
+    {
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int z = 0; z < gridSize.z; z++)
+            {
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(x, gridHeight, z), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {z}";
+
+                myRenderer =  spawnedTile.GetComponent<MeshRenderer>();
+                if ((x + z) % 2 == 1) {
+                    myRenderer.material = whiteMaterial;
+                }
+                else
+                {
+                    myRenderer.material = blackMaterial;
+                }
+            }
+        }
+    }
+
+}
+
