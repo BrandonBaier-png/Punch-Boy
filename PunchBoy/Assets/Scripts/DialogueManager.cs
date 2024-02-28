360d97109722bff3d22ca0c0dc78636533353319
BrandonBaier-png <baierb1283@my.uwstout.edu>
2024-02-28 13:27:11 -0600

    Initial attempt to add Pokemon dialogue to new scene
    Attempting to commit the Pokemon dialogue style into a new scene within within the game.
    (Unfinished)

---
+++ PunchBoy/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    // Start is called before the first frame update
+
+   
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Conversation with " + name);
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+    }
+    public void EndDialogue()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
