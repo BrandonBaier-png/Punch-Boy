bea85ddc28f371b54379211a1d99640e8c8490e6
TurnerN2002 <norumt1609@my.uwstout.edu>
2024-03-05 10:57:17 -0600

    Punch Boy Attacks
    Code from Turner, involves punch boy's attacks

---
+++ PunchBoy/Assets/Scripts/Boss.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public float bossHealth = 500;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("FireFist"))
+        {
+            bossHealth = bossHealth - 10;
+            Debug.Log(bossHealth);
+            Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("BasicPunch"))
+        {
+            bossHealth = bossHealth - 5;
+            Debug.Log(bossHealth);
+            Destroy(other.gameObject);
+        }
+    }
+}
